2bd7dc9630aad8c3d16479be70ce9e5689bbc10b
Hossein <hossenghafouri@gmail.com>
2025-09-03 13:38:30 +0400

    Update Button.js
    complete onclick event

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,6 @@
 import { useContext } from "react";
 import {CalcContext} from '../components/context/CalcContext'
 
-
 const getStyleName = (btn) => {
   const className = {
     "=": "equals",
@@ -30,13 +29,93 @@ const resetClick = () => {
 setCalc({ sign: '', num:0, res:0 })
 }
 
+// User click number
+const handleClickButton = () => {
+const numberString = value.toString();
+
+let numberValue;
+if (numberString === "0" && calc.num === 0) {
+  numberValue = "0"
+}else {
+  numberValue = Number(calc.num + numberString)
+}
+
+setCalc({
+  ...calc,
+  num:numberValue
+})
+
+}
+
+// User click operation
+const signClick = () => {
+setCalc({
+  sign: value,
+  res: !calc.res && calc.num ? calc.num : calc.res,
+  num: 0
+})
+}
+
+// User Click equals
+const equalsClick = () => {
+  if (calc.res && calc.num) {
+      const math = (a, b, sign) => {
+    const result = {
+      '+': (a, b) => a + b,
+      '-': (a, b) => a - b,
+      'x': (a, b) => a * b,
+      '/': (a, b) => a / b,
+    }
+    return result[sign](a, b)
+  }
+    setCalc({
+    res: math(calc.res, calc.num, calc.sign),
+    sign: '',
+    num: 0
+  })
+  }
+
+
+}
+
+// User Click persent
+const persentClick = () => {
+  setCalc({
+    num: (calc.num / 100),
+    res: (calc.res / 100),
+    sign: ''
+  })
+}
+
+// User Click invert btn
+const invertClick = () => {
+setCalc({
+  num: calc.num ? calc.num * -1 : 0,
+  res: calc.res ? calc.res * -1 : 0,
+  sign: ''
+})
+}
+
 
   const handleBtnClick = () => {
     const result = {
       ".": commaClick,
-      "C": resetClick
+      "C": resetClick,
+      "/": signClick,
+      "x": signClick,
+      "-": signClick,
+      "+": signClick,
+      "=": equalsClick,
+      "%": persentClick,
+      "+-": invertClick,
     };
-    return result[value]()
+    if (result[value]) {
+      return result[value]();
+    } else {
+      return handleClickButton();
+      
+    }
+    
   };
 
   return (
